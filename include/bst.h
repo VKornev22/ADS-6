f07e080e5ab24d4972f65e4cc959e7da4d6b5950
Владимир Корнев <78905217+VKornev22@users.noreply.github.com>
2021-05-31 13:42:48 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -2,4 +2,75 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
+#include <string>
+
+template <typename T>
+
+class BST {
+ public:
+  struct Node {
+  T value;
+  int count;
+  Node* left;
+  Node* right;
+  };
+
+BST(): root(nullptr) {}
+~BST() {}
+
+void add(T value) {
+  root = addNode(root, value);
+}
+int depth() {
+  return depthTree(root);
+}
+int search(T value) {
+  return searchNode(root, value);
+}
+
+  private:
+Node* root;
+Node* addNode(Node* root, T value) {
+        if (root == nullptr) {
+            root = new Node;
+            root-> value = value;
+            root-> count = 1;
+            root-> left = root-> right = nullptr;
+        } else if (root-> value > value) {
+             root-> left = addNode(root-> left, value);
+        } else if (root-> value < value) {
+             root-> right = addNode(root-> right, value);
+        } else {
+             root->count++;
+        }
+        return root;
+}
+
+int depthTree(Node* root) {
+if (root == nullptr)
+     return 0;
+if ((root->left == nullptr) && (root->right == nullptr))
+if ((root->left == nullptr) && (root->right == nullptr)) {
+     return 0;
+}
+            int lh = depthTree(root->left);
+            int rh = depthTree(root->right);
+            if (lh > rh)
+               return lh + 1;
+            else
+               return rh + 1;
+}
+
+int searchNode(Node* root, T value) {
+if (root == nullptr)
+    return 0;
+else if (root->value > value)
+    return searchNode(root->left, value);
+else if (root->value < value)
+    return searchNode(root->right, value);
+else
+    return root->count;
+}
+};
+
 #endif  // INCLUDE_BST_H_
