03f0c86fdbf608f27377559aa4946f90bba5cab3
Владимир Корнев <78905217+VKornev22@users.noreply.github.com>
2021-05-31 13:48:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,27 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+
+  BST<std::string>Tree;
+  std::string result = "";
+  char sym;
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "File error!" << std::endl;
+    return Tree;
+  }
+  while (!file.eof()) {
+    sym = file.get();
+    if ((sym >= 'A' && sym <= 'Z') || (sym >= 'a' && sym <= 'z')) {
+      if (sym >= 'A' && sym <= 'Z') {
+        sym += 32;
+      }
+      result += sym;
+    } else if (result !="") {
+      Tree.add(result);
+      result = "";
+    }
+  }
+  file.close();
+  return Tree;
 }
